2832d4eea3adee41ddf19f4d31cf4ca5e0eaadaf
Austyn Trace <atrace@gmail.com>
2017-09-04 02:17:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,4 +62,7 @@ function placeOrder(cardNumber) {
   if(!cardNumber){
     return 'Sorry, we don\'t have a credit card on file for you.'
   }
+  var toReturn = `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`;
+  cart = [];
+  return toReturn;
 }
