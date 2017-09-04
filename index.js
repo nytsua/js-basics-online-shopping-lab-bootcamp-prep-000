711bc97e29884b4b78b3361715bf1e218e914822
Austyn Trace <atrace@gmail.com>
2017-09-04 02:27:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var price = Math.floor(Math.random()*100);
-  console.log(`price: ${price}`);
-  cart.push({[item]: price})
+  cart.push({[item]: Math.floor(Math.random()*100)})
   console.log(`${item} has been added to your cart.`);
   return cart;
 }
