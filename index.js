f4ce040b7bf0a259d578f915f9d57ae7ee819598
Austyn Trace <atrace@gmail.com>
2017-09-04 02:21:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ function addToCart(item) {
 
 function viewCart() {
   if(cart.length === 0){
-    return 'Your shopping cart is empty.';
+    console.log('Your shopping cart is empty.');
   } else if(cart.length === 1){
     var product = Object.keys(cart[0])[0];
-    return `In your cart, you have ${product} at ${cart[product]}.`
+    console.log(`In your cart, you have ${product} at ${cart[product]}.`);
   } else if(cart.length === 2){
     var product1 = Object.keys(cart[0])[0];
     var product2 = Object.keys(cart[1])[0];
-    return `In your cart, you have ${product1} at ${cart[product]} and ${product2} at ${cart[product2]}.`
+    console.log(`In your cart, you have ${product1} at ${cart[product]} and ${product2} at ${cart[product2]}.`);
   } else {
     var count = 1;
     var product = Object.keys(cart[0])[0];
