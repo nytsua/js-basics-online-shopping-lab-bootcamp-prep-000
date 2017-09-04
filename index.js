c883f9ff42fb7a1a6b960ba7c6fb182a316458b7
Austyn Trace <atrace@gmail.com>
2017-09-04 02:40:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,10 +30,8 @@ function viewCart() {
     var product = Object.keys(cart[0])[0];
     var toReturn = `In your cart, you have ${product} at $${cart[0][product]}`;
     do {
-      for(let i=1;i<cart.length-2;i++){
-        var product1 = Object.keys(cart[i])[0];
-        toReturn += `, ${product1} at $${cart[i][product1]}`;
-      }
+        var product1 = Object.keys(cart[count])[0];
+        toReturn += `, ${product1} at $${cart[count][product1]}`;
       count++;
     } while(count < cart.length-1);
     var product = Object.keys(cart[count])[0];
