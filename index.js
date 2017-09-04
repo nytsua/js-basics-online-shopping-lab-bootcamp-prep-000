06e95da7fb24369b38b44aeaf9471b9a551a6edb
Austyn Trace <atrace@gmail.com>
2017-09-04 01:57:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,12 @@ function viewCart() {
   if(cart.length === 0){
     return 'Your shopping cart is empty.';
   } else if(cart.length === 1){
-    var product = Object.keys()[0];
-    return `In your cart, you have ${products[0]} at ${cart[product]}.`
+    var product = Object.keys(cart[0])[0];
+    return `In your cart, you have ${product} at ${cart[product]}.`
   } else if(cart.length === 2){
-    var product = Object.keys()[0];
-    return `In your cart, you have ${products[0]} at ${cart[0]} and ${products[1]} at ${cart[1]}.`
+    var product1 = Object.keys(cart[0])[0];
+    var product2 = Object.keys(cart[1])[0];
+    return `In your cart, you have ${product1} at ${cart[product]} and ${product2} at ${cart[product2]}.`
   } else {
     var count = 1;
     var toReturn = `In your cart, you have ${products[0]} at ${cart[0]}`
